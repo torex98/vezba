bc4fbda73fbfb84b3c477bf596a29429b3e53a96
Andrija Lepojevic <andrijalepojevic@gmail.com>
2022-04-27 14:20:46 +0200

    izbacena recenica kroz JS kod

--- js_kurs1/skripta.js
+++ js_kurs1/skripta.js
@@ -19,7 +19,7 @@ console.log('------------------');
 let najbolji = document.getElementById('recenica'); //SAMO JEDAN ELEMENT!!!
 
 console.log(najbolji.innerText);
-*/
+
 
 
 //SLEKTOVANJE SPANOVA 
@@ -30,5 +30,21 @@ console.log(brend);
 for(let auto of brend){
 	console.log(auto.innerText);
 }
+console.log('------------------');
 
-let modeli = document.querySelectorAll('model')
+
+let modeli = document.querySelectorAll('.modeli');
+
+for(let model of modeli){
+	console.log(model.innerText);
+}
+
+let recenica = document.querySelector('#recenica'); //#recenica id kroz css
+
+console.log(recenica.innerText);
+
+*/
+//BRISEMO RECENICU
+
+let recenica = document.querySelector('#recenica');
+	recenica.remove();
