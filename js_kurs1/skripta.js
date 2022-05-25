29eee1033334a5beaec544cd7f4876f22db32a78
Andrija Lepojevic <andrijalepojevic@gmail.com>
2022-05-25 14:58:11 +0200

    Update skripta.js
    Dodati komentari

--- js_kurs1/skripta.js
+++ js_kurs1/skripta.js
@@ -44,7 +44,27 @@ let recenica = document.querySelector('#recenica'); //#recenica id kroz css
 console.log(recenica.innerText);
 
 */
-//BRISEMO RECENICU
+//Selektovanje elementa po ID-u i brisanje iz HTML-a
 
 let recenica = document.querySelector('#recenica');
 	recenica.remove();
+
+//Kreiranje novog DIV elementa
+
+let novi_el = document.createElement('div');
+
+//dodavanje klase DIV elementu
+novi_el.classList ='novi_element'
+
+//dodavanje teksta elementu
+//novi_el.innerText = 'Pozdrav!!!!';
+
+novi_el.innerHTML = '<hr> POZDRAV!!!';
+
+let body = document.querySelector('body');
+body.appendChild(novi_el);
+
+
+
+
+
